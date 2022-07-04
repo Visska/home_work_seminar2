850fbdfa0caae024fc1c6aed9e749c12e4e2b6e0
Visska <strannica.ev@gmail.com>
2022-07-04 12:49:43 +0300

    add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿int three_digit_number = new Random().Next (100, 1000);
+Console.WriteLine($"Вводимое число {three_digit_number}");
+int second_number = getFindingSecondNumber(three_digit_number);
+Console.WriteLine($"Вторая цифра числа {second_number}");
+
+
+int getFindingSecondNumber(int number)
+{
+    return ( ( number % 100 ) / 10);
+}
