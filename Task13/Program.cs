f194ff668cfd79b7d4dd3bf2cd43959d0a6df8d0
Visska <strannica.ev@gmail.com>
2022-07-04 13:29:01 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿int random_number = new Random().Next(1, 1011);
+Console.WriteLine($"Введенное число {random_number}");
+int number3 = -1;
+
+while(random_number > 99)
+{
+    number3 = random_number % 10;
+    random_number = random_number / 10;
+}
+
+if (number3 == -1)
+{
+    Console.WriteLine($"Третьей цифры нет.");
+}
+else
+{
+    Console.WriteLine($"Третья цифра = {number3}");
+}
