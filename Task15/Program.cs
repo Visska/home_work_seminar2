d6f80fc460343ac83432c86291c2d4638dc3abef
Visska <strannica.ev@gmail.com>
2022-07-04 13:49:31 +0300

    add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,10 @@
+﻿int day_week = new Random().Next(1, 8);
+Console.WriteLine($"День недели от 1 до 7 -> {day_week}");
+if(day_week <= 5)
+{
+    Console.WriteLine($"{day_week} - нет, не выходной");
+}
+else
+{
+    Console.WriteLine($"{day_week} - да, выходной");
+}
